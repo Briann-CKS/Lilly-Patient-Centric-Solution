5f62e2ee3b6e1dd5d9966f78fbf13de6c3cb44ec
Cheong, Brian Kean Shiong <brcheong@iu.edu>
2023-07-11 15:17:23 -0400

    added sign up page

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 const express = require ("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const User = require('../models/user')
-const mongoose = require ("mongoose");
+const User = require('./models/user')
+// const mongoose = require ("mongoose");
 
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+// mongoose.set('strictQuery', true);
+// mongoose.connect(process.env.DATABASE_URL)
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
-// const usersRouter = require('./routes/users')
-// app.use('/users', usersRouter)
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
 
 app.use(express.static(__dirname + '/public'));
 
@@ -28,14 +28,15 @@ app.get('/login', function (req, res) {
     res.sendFile(__dirname + "/html/login.html");
 });
 
-// app.post('/signup', function (req, res) {
-//     const email = req.body.email;
-//     const name = req.body.name;
-//     const username = req.body.username;
-//     const password = req.body.password;
+app.post('/signup', function (req, res) {
+    // const email = req.body.email;
+    // const name = req.body.name;
+    // const username = req.body.username;
+    // const password = req.body.password;
 
-// });
+    // console.log("Testing");
+    res.sendFile(__dirname + "/html/chatbot.html");
 
-
+});
 
 app.listen(PORT, () => console.log('Server started'));
