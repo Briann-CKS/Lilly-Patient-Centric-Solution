2383324f15a71c5bfda476292bbf2041a8c19426
Gabriel Iskandar <gabriel.iskandar@lilly.com>
2023-07-11 14:37:35 -0400

    Updated Style to include new chat bubbles, left and right aligned

--- models/chatbot.js
+++ models/chatbot.js
@@ -9,10 +9,15 @@ function sendQuestion() {
     // For example, you can display it in the chat container as a user bubble
     const chatContainer = document.querySelector(".chat-container");
     const userBubble = document.createElement("div");
-    userBubble.classList.add("user-bubble");
+    userBubble.setAttribute("class", "user-bubble bubble");
     userBubble.textContent = userInput;
     chatContainer.appendChild(userBubble);
-  
+
+    const botBubble = document.createElement("div");
+    botBubble.setAttribute("class", "bot-bubble bubble");
+    botBubble.textContent = "I cannot answer your question at the moment.";
+    chatContainer.appendChild(botBubble);
+
     // Clear the input field after sending the question
     document.getElementById("userInput").value = "";
   }
