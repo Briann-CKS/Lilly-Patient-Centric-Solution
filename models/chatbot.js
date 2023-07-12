fe2b3defdfcc143be1ff92734669535ca0bf955d
Gabriel Iskandar <gabriel.iskandar@lilly.com>
2023-07-12 09:12:29 -0400

    Added enter key send

--- models/chatbot.js
+++ models/chatbot.js
@@ -6,6 +6,10 @@ LILLY_CHAT_API = "https://chat.apps.lrl.lilly.com/ask/cliniclilly"
 function sendQuestion() {
     // Get the user input from the input field
     const userInput = document.getElementById("userInput").value;
+
+    if (userInput == "") {
+        return
+    }
   
     // Perform any necessary actions with the user's query
     // For example, you can display it in the chat container as a user bubble
@@ -43,4 +47,10 @@ function sendQueryLillyChat(input) {
         // Handle any errors
         console.error(error);
       });
+}
+
+function pressKey(event) {
+    if(event.keyCode == 13) {
+        sendQuestion()
+    }
 }
