a967477645b8af84497a4c26073f3870f9f6f602
Brian Cheong <brian.cheong@lilly.com>
2023-07-11 23:03:54 -0400

    modified routing, connected chatbot.js, and modified API request function

---
+++ public/js/chatbot.js
@@ -0,0 +1,68 @@
+const LILLY_CHAT_API = "https://chat.apps.lrl.lilly.com/ask/cliniclilly"
+
+function sendQuestion() {
+    // Get the user input from the input field
+    const userInput = document.getElementById("userInput").value;
+  
+    // Perform any necessary actions with the user's query
+    // For example, you can display it in the chat container as a user bubble
+    const chatContainer = document.querySelector(".chat-container");
+    const userBubble = document.createElement("div");
+    userBubble.setAttribute("class", "user-bubble bubble");
+    userBubble.textContent = userInput;
+    chatContainer.appendChild(userBubble);
+
+    const botBubble = document.createElement("div");
+    botBubble.setAttribute("class", "bot-bubble bubble");
+    // botBubble.textContent = "I cannot answer your question at the moment.";
+    // chatContainer.appendChild(botBubble);
+
+    // Clear the input field after sending the question
+    document.getElementById("userInput").value = "";
+
+    // Create a new URLSearchParams object
+    const params = new URLSearchParams();
+
+    // Add the user input as a parameter
+    params.append("query", userInput);
+
+    // Get the query string
+    const queryString = params.toString();
+
+    // Create the API endpoint URL with the query string
+    const apiUrl = "https://chat.apps.lrl.lilly.com/ask/cliniclilly?q=What is a clinical trial" + queryString;
+
+    // Use the apiUrl in your API request
+    // ...
+    // Access the query parameters using the searchParams property
+    const url = new URL("https://chat.apps.lrl.lilly.com/ask/cliniclilly?q=What is a clinical trial");
+    const paramsFromUrl = url.searchParams;
+
+    // Get the value of a specific parameter
+    const param1Value = paramsFromUrl.get("param1");
+    console.log(param1Value); // Output: "value1"
+
+    sendQueryLillyChat(userInput);
+    botBubble.textContent = "I am not smart enough.";
+    chatContainer.appendChild(botBubble);
+}
+
+function sendQueryLillyChat(input) {
+    const query = { q: input };
+    const params = new URLSearchParams(query).toString();;
+    // const url = `${LILLY_CHAT_API}?${params}`;
+    const url = "https://chat.apps.lrl.lilly.com/ask/cliniclilly?q=What%20is%20a%20clinical%20trial";
+
+    console.log(url);
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data
+        alert(data);
+      })
+      .catch(error => {
+        // Handle any errors
+        alert(error);
+      });
+}
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const express = require ("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 const User = require('./models/user')
-// const mongoose = require ("mongoose");
+const mongoose = require ("mongoose");
 
-// mongoose.set('strictQuery', true);
-// mongoose.connect(process.env.DATABASE_URL)
-// const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
 const usersRouter = require('./routes/users')
 app.use('/users', usersRouter)
 
@@ -28,15 +30,35 @@ app.get('/login', function (req, res) {
     res.sendFile(__dirname + "/html/login.html");
 });
 
-app.post('/signup', function (req, res) {
-    // const email = req.body.email;
-    // const name = req.body.name;
-    // const username = req.body.username;
-    // const password = req.body.password;
-
-    // console.log("Testing");
+app.get('/chatbot', function (req, res) {
     res.sendFile(__dirname + "/html/chatbot.html");
+})
 
+app.post('/signup', function (req, res) {
+    const Email = req.body.email;
+    const FullName = req.body.name;
+    const Username = req.body.username;
+    const Password = req.body.password;
+
+    console.log(Email);
+    console.log(FullName);
+    console.log(Username);
+    console.log(Password);
+    res.redirect("/chatbot");
+
+});
+
+app.post('/login', function (req, res) {
+    const Email = req.body.email;
+    const FullName = req.body.name;
+    const Username = req.body.username;
+    const Password = req.body.password;
+
+    console.log(Email);
+    console.log(FullName);
+    console.log(Username);
+    console.log(Password);
+    res.redirect("/chatbot");
 });
 
 app.listen(PORT, () => console.log('Server started'));
