901787b6b4966a58f367ef398d231e63258713cb
Brian Cheong <brian.cheong@lilly.com>
2023-07-12 14:01:02 -0400

    updated files

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -1,12 +1,11 @@
 // require ('dotenv').config();
-// const { spawn } = require('child_process');
-// const { OpenAI } = require ("langchain/llms/openai");
-
-const LILLY_CHAT_API = "https://chat.apps.lrl.lilly.com/ask/cliniclilly"
+// const { OpenAI } = require ("langchain/llms/openai");s
+import { OpenAI } from '../../dist/llms/openai.js';
+// const LILLY_CHAT_API = "https://chat.apps.lrl.lilly.com/ask/cliniclilly"
 
 function sendQuestion() {
-  require ('dotenv').config();
-  const { OpenAI } = require ("langchain/llms/openai");
+  // require ('dotenv').config();
+  // const { OpenAI } = require ("langchain/llms/openai");
 
   const model = new OpenAI({
     // azureOpenAIApiKey: "92c5be9400184e13b741b2ce87196b6f",
@@ -15,52 +14,34 @@ function sendQuestion() {
     // azureOpenAIApiVersion: "2023-05-15",
     temperature: 0.0,
   });
-    // Get the user input from the input field
-    const userInput = document.getElementById("userInput").value;
+
+  // Get the user input from the input field
+  const userInput = document.getElementById("userInput").value;
+
+  if (userInput == "") {
+    return
+  }
   
-    // Perform any necessary actions with the user's query
-    // For example, you can display it in the chat container as a user bubble
-    const chatContainer = document.querySelector(".chat-container");
-    const userBubble = document.createElement("div");
-    userBubble.setAttribute("class", "user-bubble bubble");
-    userBubble.textContent = userInput;
-    chatContainer.appendChild(userBubble);
+  // Perform any necessary actions with the user's query
+  // For example, you can display it in the chat container as a user bubble
+  const chatContainer = document.querySelector(".chat-container");
+  const userBubble = document.createElement("div");
+  userBubble.setAttribute("class", "user-bubble bubble");
+  userBubble.textContent = userInput;
+  chatContainer.appendChild(userBubble);
 
-    const botBubble = document.createElement("div");
-    botBubble.setAttribute("class", "bot-bubble bubble");
-    // botBubble.textContent = "I cannot answer your question at the moment.";
-    // chatContainer.appendChild(botBubble);
+  const botBubble = document.createElement("div");
+  botBubble.setAttribute("class", "bot-bubble bubble");
+  // botBubble.textContent = "I cannot answer your question at the moment.";
+  // chatContainer.appendChild(botBubble);
 
-    // Clear the input field after sending the question
-    document.getElementById("userInput").value = "";
+  // Clear the input field after sending the question
+  document.getElementById("userInput").value = "";
 
-    // // Create a new URLSearchParams object
-    // const params = new URLSearchParams();
-
-    // // Add the user input as a parameter
-    // params.append("query", userInput);
-
-    // // Get the query string
-    // const queryString = params.toString();
-
-    // // Create the API endpoint URL with the query string
-    // const apiUrl = "https://chat.apps.lrl.lilly.com/ask/cliniclilly?q=What is a clinical trial" + queryString;
-
-    // // Use the apiUrl in your API request
-    // // ...
-    // // Access the query parameters using the searchParams property
-    // const url = new URL("https://chat.apps.lrl.lilly.com/ask/cliniclilly?q=What is a clinical trial");
-    // const paramsFromUrl = url.searchParams;
-
-    // // Get the value of a specific parameter
-    // const param1Value = paramsFromUrl.get("param1");
-    // console.log(param1Value); // Output: "value1"
-
-    console.log("gabe is smart");
-    sendQueryLillyChat(userInput, botBubble, model);
-    
-    // botBubble.textContent = "I am not smart enough.";
-    // chatContainer.appendChild(botBubble);
+  sendQueryLillyChat(userInput, botBubble, model);
+  
+  // botBubble.textContent = "I am not smart enough.";
+  // chatContainer.appendChild(botBubble);
 }
 
 async function sendQueryLillyChat(input, bubble, model) {
